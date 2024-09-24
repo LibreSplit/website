5fa3d1c28ff0b10cb9e3b9fbef243b2e54577d6e
Loomeh <loombadoop@gmail.com>
2024-09-24 15:24:58 +0100

    Update AppImage and Debuntu build instructions

--- src/app/installation/page.tsx
+++ src/app/installation/page.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import CodeBlock from "../components/CodeBlock";
 import DownloadButton from "../components/DownloadButton";
-import { Button } from "@/components/ui/button";
 
-const FILE_URL:string = "https://libresplit.loomeh.tech/files/LibreSplit-x86_64.AppImage";
+const FILE_URL:string = "https://libresplit.loomeh.tech/files/LibreSplit-latest-x86_64.AppImage";
 
 export default function Home() {
   return (
     <>
       <div>
-        <h1><b>AppImage</b></h1><br/>
+        <h1><b>AppImage (Sept. 24, 2024 - b56ce37)</b></h1><br/>
         <DownloadButton fileUrl={FILE_URL}/>
       </div>
       <br/><br/>
@@ -30,7 +29,7 @@ export default function Home() {
       <div>
         <h1><b>Debian/Ubuntu (Source)</b></h1>
         <CodeBlock>
-          sudo apt -y install git libgtk-3-dev libx11-dev libjansson-dev libluajit-5.1-dev<br/>
+          sudo apt -y install build-essential git libgtk-3-dev libx11-dev libjansson-dev libluajit-5.1-dev<br/>
           git clone https://github.com/wins1ey/LibreSplit --recurse-submodules<br/>
           cd LibreSplit<br/>
           make<br/>
