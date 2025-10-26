44330ff43b8cd35f7eea53a451943fbebe224024
Zane Holbrooke-Jones <67561520+Loomeh@users.noreply.github.com>
2025-10-26 20:50:48 +0000

    Update Git URL

--- src/app/installation/page.tsx
+++ src/app/installation/page.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import CodeBlock from "../components/CodeBlock";
-import DownloadButton from "../components/DownloadButton";
-
-const FILE_URL:string = "https://libresplit.loomeh.tech/files/LibreSplit-latest-x86_64.AppImage";
 
 export default function Home() {
   return (
@@ -25,7 +22,7 @@ export default function Home() {
         <h1><b>Debian/Ubuntu (Source)</b></h1>
         <CodeBlock>
           sudo apt -y install build-essential git libgtk-3-dev libx11-dev libjansson-dev libluajit-5.1-dev<br/>
-          git clone https://github.com/wins1ey/LibreSplit --recurse-submodules<br/>
+          git clone https://github.com/LibreSplit/LibreSplit --recurse-submodules<br/>
           cd LibreSplit<br/>
           make<br/>
           sudo make install<br/>
